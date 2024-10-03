7b7a101c48816dae789bb1b25e902d0e3c4d4838
Mohamed Ouhami <mohamed.ouhami2001@gmail.com>
2024-10-03 17:34:54 +0100

    Finished the authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,46 @@
-import React from 'react'
-import './globals.css';
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
+export default function Home() {
+  const { status } = useSession();
+  const router = useRouter();
 
-const page = () => {
+  const showSession = () => {
+    if (status === "authenticated") {
+      return (
+        <button
+          className="border border-solid border-black rounded"
+          onClick={() => {
+            signOut({ redirect: false }).then(() => {
+              router.push("/");
+            });
+
+          }}
+        >
+          Sign Out
+        </button>
+      )
+    } else if (status === "loading") {
+      return (
+        <span className="text-[#888] text-sm mt-7">Loading...</span>
+      )
+    } else {
+      return (
+        <Link
+          href="/login"
+          className="border border-solid border-black rounded"
+        >
+          Sign In
+        </Link>
+      )
+    }
+  }
   return (
-    <div>
-      The dashboard
-      <a href="/api/auth/logout">Logout</a>
-    </div>
-  )
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-xl">Home</h1>
+      {showSession()}
+    </main>
+  );
 }
-
-export default page
